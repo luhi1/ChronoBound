104686039169edecf4def5d9ef1347917e54711e
lulhi <michaelborov2007@gmail.com>
2022-04-22 17:19:45 -0700

    Added the icon and fixed the Admin toolbar.

--- app/src/main/java/com/example/userauth/adminCalendarActivity.kt
+++ app/src/main/java/com/example/userauth/adminCalendarActivity.kt
@@ -2,10 +2,18 @@ package com.example.userauth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class adminCalendarActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin_calendar)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navController = findNavController(R.id.fragmentContainerView)
+
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
