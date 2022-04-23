dc35fc11c7349bad4f2e8cc68844d4fd68b9b0bd
lulhi <michaelborov2007@gmail.com>
2022-04-22 21:50:00 -0700

    Added the Log Out and Bug Buttons.

--- app/src/main/java/com/example/userauth/CalendarActivity.kt
+++ app/src/main/java/com/example/userauth/CalendarActivity.kt
@@ -1,10 +1,21 @@
 package com.example.userauth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.*
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
 
 class CalendarActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,7 +24,51 @@ class CalendarActivity : AppCompatActivity() {
 
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         val navController = findNavController(R.id.fragmentStorage)
+        val fragmentLayout = findViewById<View>(R.id.fragmentStorage)
+        val bugButton = findViewById<ImageView>(R.id.bugButton)
+        val saveBugButton = findViewById<Button>(R.id.saveBugButton)
+        val backBugButton = findViewById<Button>(R.id.backBugButton)
+        val exitButton = findViewById<ImageView>(R.id.exitButton)
+        val bugLayout = findViewById<LinearLayout>(R.id.bugLayout)
+        val bugText = findViewById<EditText>(R.id.etBug)
 
         bottomNavigationView.setupWithNavController(navController)
+
+        exitButton.setOnClickListener(){
+            Firebase.auth.signOut()
+            startActivity(Intent(this@CalendarActivity, LoginActivity::class.java))
+        }
+        bugButton.setOnClickListener(){
+            bugText.setText("")
+            fragmentLayout.visibility = View.INVISIBLE
+            bugLayout.visibility = View.VISIBLE
+            bugButton.visibility = View.INVISIBLE
+        }
+        saveBugButton.setOnClickListener(){
+            FirebaseDatabase.getInstance().getReference("bugs").child("bugNumber").addListenerForSingleValueEvent(object:
+                ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val intValue = snapshot.getValue<Int>()
+                    val value = (intValue!! +1)
+                    FirebaseDatabase.getInstance().getReference("bugs").child("bug$value").setValue(bugText.text.toString())
+                    FirebaseDatabase.getInstance().getReference("bugs").child("bugNumber").setValue(intValue+1)
+                    fragmentLayout.visibility = View.VISIBLE
+                    bugLayout.visibility = View.INVISIBLE
+                    bugButton.visibility = View.VISIBLE
+
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    Toast.makeText(this@CalendarActivity, "Failed to connect to the database. Check your network connection.", Toast.LENGTH_LONG)
+                    Log.d("joe", "Failed to read data.")
+                }
+
+            })
+        }
+        backBugButton.setOnClickListener(){
+            fragmentLayout.visibility = View.VISIBLE
+            bugLayout.visibility = View.INVISIBLE
+            bugButton.visibility = View.VISIBLE
+        }
     }
 }
--- app/src/main/java/com/example/userauth/adminCalendarActivity.kt
+++ app/src/main/java/com/example/userauth/adminCalendarActivity.kt
@@ -1,10 +1,25 @@
 package com.example.userauth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.ktx.Firebase
 
 class adminCalendarActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,7 +28,14 @@ class adminCalendarActivity : AppCompatActivity() {
 
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         val navController = findNavController(R.id.fragmentContainerView)
+        val exitButton = findViewById<ImageView>(R.id.exitButton)
+
+        exitButton.setOnClickListener(){
+            Firebase.auth.signOut()
+            startActivity(Intent(this@adminCalendarActivity, LoginActivity::class.java))
+        }
 
         bottomNavigationView.setupWithNavController(navController)
+
     }
 }
